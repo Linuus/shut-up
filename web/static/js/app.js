17ca753e464606c16ce8a8db0f440a2c5cc29679
Linus Pettersson <linus.pettersson@gmail.com>
2016-12-12 21:55:03 +0100

    Semicolons

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,63 +1,63 @@
-import "phoenix_html"
-import {Socket, Presence} from "phoenix"
-import $ from "jquery"
+import "phoenix_html";
+import {Socket, Presence} from "phoenix";
+import $ from "jquery";
 
-let $status = $("#status")
-let $messages = $("#messages")
-let $input = $("#message-input")
-let $username = $("#username")
-let $usernameContainer = $("#username-container")
+let $status = $("#status");
+let $messages = $("#messages");
+let $input = $("#message-input");
+let $username = $("#username");
+let $usernameContainer = $("#username-container");
 
-$status.hide()
-$messages.hide()
-$input.hide()
+$status.hide();
+$messages.hide();
+$input.hide();
 
 $username.off("keypress").on("keypress", e => {
   if (e.keyCode == 13) {
-    initiateChat($username.val())
+    initiateChat($username.val());
   }
-})
+});
 
 let initiateChat = (username) => {
-  let socket = new Socket("/socket", { params: {username: username} })
+  let socket = new Socket("/socket", { params: {username: username} });
 
-  socket.connect()
+  socket.connect();
 
-  $usernameContainer.hide()
-  $status.show()
-  $messages.show()
-  $input.show()
-  $input.focus()
+  $usernameContainer.hide();
+  $status.show();
+  $messages.show();
+  $input.show();
+  $input.focus();
 
-  let sanitize = (html) => { return $("<div/>").text(html).html() }
+  let sanitize = (html) => { return $("<div/>").text(html).html(); };
 
   let messageTemplate = (msg) => {
-    let body = sanitize(msg.body)
-    let user = sanitize(msg.user || "anonymous")
+    let body = sanitize(msg.body);
+    let user = sanitize(msg.user || "anonymous");
 
-    return (`<p><a href='#'>[${user}]</a> ${body}</p>`)
-  }
+    return (`<p><a href='#'>[${user}]</a> ${body}</p>`);
+  };
 
-  let chan = socket.channel("room:lobby", {})
+  let chan = socket.channel("room:lobby", {});
   chan.join().receive("ok", () => console.log("join ok"))
-             .receive("timeout", () => console.log("Connection interruption"))
-  chan.onError(e => console.log("something went wrong", e))
-  chan.onClose(e => console.log("channel closed", e))
+    .receive("timeout", () => console.log("Connection interruption"));
+  chan.onError(e => console.log("something went wrong", e));
+  chan.onClose(e => console.log("channel closed", e));
 
   $input.off("keypress").on("keypress", e => {
     if (e.keyCode == 13) {
-      chan.push("new:msg", {body: $input.val()})
-      $input.val("")
+      chan.push("new:msg", {body: $input.val()});
+      $input.val("");
     }
-  })
+  });
 
   chan.on("new:msg", msg => {
-    $messages.append(messageTemplate(msg))
-    scrollTo(0, document.body.scrollHeight)
-  })
+    $messages.append(messageTemplate(msg));
+    scrollTo(0, document.body.scrollHeight);
+  });
 
   chan.on("user:entered", msg => {
-    let user = sanitize(msg.user || "anonymous")
-    $messages.append(`<br/><i>[${user} entered]</i>`)
-  })
-}
+    let user = sanitize(msg.user || "anonymous");
+    $messages.append(`<br/><i>[${user} entered]</i>`);
+  });
+};
